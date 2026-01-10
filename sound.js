111c763a12cfa122cec7bf80a0782f06175d841a
troop7876-cmyk <troop7876@gmail.com>
2026-01-10 23:34:29 +0530

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,15 +1,18 @@
+// Click / Win / Lose sounds
 const clickSound = new Audio("https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg");
 const winSound = new Audio("https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg");
 const loseSound = new Audio("https://actions.google.com/sounds/v1/cartoon/boing.ogg");
 
-function playClick(){
-  clickSound.play();
-}
+// Background music
+const bgMusic = new Audio("https://cdn.pixabay.com/audio/2022/03/15/audio_7c3a7b0d7f.mp3");
+bgMusic.loop = true;
+bgMusic.volume = 0.4;
 
-function playWin(){
-  winSound.play();
-}
+function playClick(){ clickSound.play(); }
+function playWin(){ winSound.play(); }
+function playLose(){ loseSound.play(); }
 
-function playLose(){
-  loseSound.play();
+function toggleMusic(){
+  if(bgMusic.paused){ bgMusic.play(); }
+  else{ bgMusic.pause(); }
 }
