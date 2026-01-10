2f6150aa104d43dd786acf171c24a1f989e9683e
troop7876-cmyk <troop7876@gmail.com>
2026-01-10 22:42:49 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,23 +1,51 @@
+let selected = [];
 let score = 0;
-let current = [];
+let time = 60;
+let timer;
 
 function loadQuiz(){
-  current = questions.sort(()=>0.5-Math.random()).slice(0,10);
-  let html="";
-  current.forEach((q,i)=>{
-    html+=`<p>${i+1}. ${q.q}</p>`;
+  selected = questions.sort(() => 0.5 - Math.random()).slice(0, 10);
+  let html = "";
+
+  selected.forEach((q,i)=>{
+    html += `<p><b>${i+1}. ${q.q}</b></p>`;
     q.a.forEach((op,j)=>{
-      html+=`<input type="radio" name="q${i}" value="${j}">${op}<br>`;
+      html += `
+        <label>
+          <input type="radio" name="q${i}" value="${j}"> ${op}
+        </label><br>`;
     });
   });
-  document.getElementById("quiz").innerHTML=html;
+
+  document.getElementById("quiz").innerHTML = html;
+  startTimer();
 }
-loadQuiz();
+
+function startTimer(){
+  timer = setInterval(()=>{
+    time--;
+    document.getElementById("time").innerText = time;
+    if(time <= 0){
+      clearInterval(timer);
+      submitQuiz();
+    }
+  },1000);
+}
 
 function submitQuiz(){
-  current.forEach((q,i)=>{
-    let sel=document.querySelector(`input[name=q${i}]:checked`);
-    if(sel && parseInt(sel.value)==q.c) score++;
+  clearInterval(timer);
+  score = 0;
+
+  selected.forEach((q,i)=>{
+    let ans = document.querySelector(`input[name=q${i}]:checked`);
+    if(ans && parseInt(ans.value) === q.c){
+      score++;
+    }
   });
-  alert("Your Score: "+score+"/10");
+
+  saveScore(score);
+  alert("Quiz Finished!\nScore: " + score + "/10");
+  window.location.href = "leaderboard.html";
 }
+
+loadQuiz();
