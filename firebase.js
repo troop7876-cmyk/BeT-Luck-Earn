1e413b84fadc179616968cd189889b368728df98
troop7876-cmyk <troop7876@gmail.com>
2026-01-10 23:13:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,15 @@
-// ADD YOUR FIREBASE CONFIG HERE
+// Firebase SDK
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID"
+  apiKey: "AIzaSyAmJtO_ucih8U9KBf4tgFDM...",
+  authDomain: "bet-luck-earn.firebaseapp.com",
+  projectId: "bet-luck-earn",
+  storageBucket: "bet-luck-earn.appspot.com",
+  messagingSenderId: "1029867508970",
+  appId: "1:1029867508970:web:72ca7c13b2004927d33393",
+  measurementId: "G-7FHXTMS67M"
 };
+
 firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
