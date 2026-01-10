43622c505e461d3318dacbf94521ba6130688818
troop7876-cmyk <troop7876@gmail.com>
2026-01-10 23:16:18 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,17 @@
-// Firebase SDK
+// Firebase CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAmJtO_ucih8U9KBf4tgFDM...",
+  apiKey: "YOUR_API_KEY",
   authDomain: "bet-luck-earn.firebaseapp.com",
   projectId: "bet-luck-earn",
   storageBucket: "bet-luck-earn.appspot.com",
   messagingSenderId: "1029867508970",
-  appId: "1:1029867508970:web:72ca7c13b2004927d33393",
-  measurementId: "G-7FHXTMS67M"
+  appId: "1:1029867508970:web:72ca7c13b2004927d33393"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.firestore();
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
