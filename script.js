3002a1ac878925d51b818134b2754bbb82c47582
troop7876-cmyk <troop7876@gmail.com>
2026-01-11 13:05:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-<script>
-const quizBank = [
-  {q:"5 + 7 = ?", a:12},
-  {q:"10 - 3 = ?", a:7},
-  {q:"Capital of India?", a:"delhi"},
-  {q:"Sun is a ___?", a:"star"},
-  {q:"Water formula?", a:"h2o"},
-  {q:"15 + 25 = ?", a:40},
-  {q:"Opposite of hot?", a:"cold"},
-  {q:"Earth revolves around?", a:"sun"}
-];
-</script>
+function signup(){
+  play("click");
+  let u=username.value, p=password.value;
+  if(!u||!p){msg.innerText="Fill all fields";return;}
+  localStorage.setItem("user_"+u,p);
+  localStorage.setItem("wallet_"+u,0);
+  msg.innerText="Signup successful. Login now.";
+}
+
+function login(){
+  play("click");
+  let u=username.value, p=password.value;
+  if(localStorage.getItem("user_"+u)==p){
+    localStorage.setItem("loggedUser",u);
+    location="dashboard.html";
+  }else msg.innerText="Wrong username or password";
+}
